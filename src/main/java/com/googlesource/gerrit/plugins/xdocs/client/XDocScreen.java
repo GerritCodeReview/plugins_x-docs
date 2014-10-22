41b61945ab76ce4359facc16d3450fdcae303d54
Edwin Kempin <edwin.kempin@sap.com>
2014-10-22 10:23:35 +0200

    Show proper error message in UI when document cannot be loaded
    Instead of showing a broken iframe, display a proper error message
    when a document cannot be loaded.
    
    This is a preparation step to add special handling if the document is
    not found, e.g. in this case we may want to display a 'Create' button
    to create the missing document.
    
    Change-Id: Ic0b7383da84f2351c24ae49eccc7c8f6fc421c9d
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/xdocs/client/XDocScreen.java
+++ src/main/java/com/googlesource/gerrit/plugins/xdocs/client/XDocScreen.java
@@ -16,6 +16,11 @@ package com.googlesource.gerrit.plugins.xdocs.client;
 
 import com.google.gerrit.plugin.client.Plugin;
 import com.google.gerrit.plugin.client.screen.Screen;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -45,17 +50,43 @@ public class XDocScreen extends VerticalPanel {
   }
 
   XDocScreen(String projectName, String revision, String fileName) {
+    setStyleName("xdocs-panel");
+
     HorizontalPanel p = new HorizontalPanel();
     p.add(new InlineHyperlink(projectName, "/admin/projects/" + projectName));
     p.add(new Label(" / " + fileName + " (" + revision + ")"));
     add(p);
 
-    setStyleName("xdocs-panel");
-    String frameId = "xdoc_iframe";
-    Frame frame = new Frame(getUrl(projectName, revision, fileName));
-    frame.getElement().setId(frameId);
-    resize(frame, frameId);
-    add(frame);
+    final String url = getUrl(projectName, revision, fileName);
+    RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+    try {
+      builder.sendRequest(null, new RequestCallback() {
+        public void onResponseReceived(Request request, Response response) {
+          int status = response.getStatusCode();
+          if (200 <= status && status < 300) {
+            String frameId = "xdoc_iframe";
+            Frame frame = new Frame(url);
+            frame.getElement().setId(frameId);
+            resize(frame, frameId);
+            add(frame);
+          } else {
+            showError(status + " " + response.getStatusText());
+          }
+        }
+
+        public void onError(Request request, Throwable exception) {
+          showError(exception.getMessage());
+        }
+      });
+    } catch (RequestException e) {
+      showError(e.getMessage());
+    }
+  }
+
+  private void showError(String message) {
+    Label l = new Label("Error on loading document: " + message);
+    l.setStyleName("xdocs-error");
+    add(l);
   }
 
   private String getUrl(String projectName, String revision, String fileName) {
