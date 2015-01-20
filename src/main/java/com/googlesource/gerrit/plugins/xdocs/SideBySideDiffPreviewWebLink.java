de63fd1a746a971d04168d03b25df31c8d49c8db
Edwin Kempin <edwin.kempin@sap.com>
2015-01-20 16:43:26 +0100

    Move preview web link classes into the server package
    They don't belong into the client package since they are only used on
    server side.
    
    Change-Id: I7940aae5c010cb9a88f2d7213e4cac3e4081adfd
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/xdocs/SideBySideDiffPreviewWebLink.java
+++ src/main/java/com/googlesource/gerrit/plugins/xdocs/SideBySideDiffPreviewWebLink.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.xdocs.client;
+package com.googlesource.gerrit.plugins.xdocs;
 
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.common.DiffWebLinkInfo;
