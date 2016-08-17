7fabf3f0a6da1c2a25476b92ae144ef94564d8cc
Sebastian Schuberth <sschuberth@gmail.com>
2016-08-17 10:57:03 +0200

    xdocs: Add a unit test for the MarkdownFormatter
    Change-Id: Ib41322474fb4df9fdfe1a3da98a58ee65dc7be23

---
+++ src/test/java/com/googlesource/gerrit/plugins/x-docs/MarkdownFormatterTest.java
@@ -0,0 +1,53 @@
+package com.googlesource.gerrit.plugins.xdocs.formatter;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertEquals;
+
+import com.googlesource.gerrit.plugins.xdocs.ConfigSection;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MarkdownFormatterTest {
+
+  private static final String PROLOG = "<html><head><style type=\"text/css\">\n\n</style></head><body>\n<p>";
+  private static final String EPILOG = "</p>\n</body></html>";
+
+  private FormatterUtil util;
+  private ConfigSection cfg;
+  private Formatters formatters;
+  private MarkdownFormatter formatter;
+
+  @Before
+  public void setUp() throws IOException {
+    util = createNiceMock(FormatterUtil.class);
+
+    // For easier result comparison, avoid the internal MarkdownFormatter to apply the default CSS.
+    expect(util.getInheritedCss(anyString(), anyString(), anyString(), anyString())).andReturn("");
+
+    replay(util);
+
+    cfg = createNiceMock(ConfigSection.class);
+
+    // Do not expect any behavior from the ConfigSection itself.
+    replay(cfg);
+
+    formatters = createNiceMock(Formatters.class);
+
+    // Avoid a NPE by just returning the ConfigSection mock object.
+    expect(formatters.getFormatterConfig(anyString(), anyString())).andReturn(cfg);
+
+    replay(formatters);
+
+    formatter = new MarkdownFormatter(util, formatters);
+  }
+
+  @Test
+  public void basicTextFormattingWorks() throws IOException {
+    String raw = "*italic* **bold** `monospace`";
+    String formatted = PROLOG + "<em>italic</em> <strong>bold</strong> <code>monospace</code>" + EPILOG;
+    assertEquals(formatter.format(null, null, null, null, cfg, raw), formatted);
+  }
+}
