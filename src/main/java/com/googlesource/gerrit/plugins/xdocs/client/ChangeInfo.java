29008bd2c9bec3ad561ff8de35a6385f6a311382
David Pursehouse <dpursehouse@collab.net>
2018-06-17 21:26:39 +0900

    ChangeInfo: Remove reference to DRAFT
    Change-Id: If806fff91fb8ac6581df97665d93b0135e17399a

--- src/main/java/com/googlesource/gerrit/plugins/xdocs/client/ChangeInfo.java
+++ src/main/java/com/googlesource/gerrit/plugins/xdocs/client/ChangeInfo.java
@@ -41,7 +41,6 @@ public class ChangeInfo extends JavaScriptObject {
   public final boolean isOpen() {
     switch (getStatus()) {
       case NEW:
-      case DRAFT:
         return true;
       default:
         return false;
